5cc682f35c68be2a10170441944debf8f1a59e06
Alicia <aliciayu22@gmail.com>
2024-06-09 16:54:27 -0600

    complete array map method lab

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+// declare const  tutorials = data
+
+// declare titlecased as const titleCased = () => { tutorials as data}
+
+// input something into data like words and index
+  // declare each sentences/array in to a variable
+  // iterate through each word
+  // update the first letter of each word to be capitalize
+
+// string.prototype.split() src: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+//ex: let sent = "these are just some words on paper"
+//    sent.split(' ').map ( ([h, ...t]) => h.toUpperCase() + t.join('').toLowerCase() )
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -11,6 +24,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+function titleCased () {
+  return tutorials.map((title) => {
+    return title.split(' ').map( ([firstLetter, ...otherLetters]) => {
+      return [firstLetter.toUpperCase() + otherLetters.join('')];
+    }).join(' ');
+  });
+};
+
